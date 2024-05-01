43ca55a2344ae3166ad7c5bb077f5dbd51b79bd7
Aman Kumar Thakur <ssamanthakur@gmail.com>
2024-05-01 13:01:04 +0530

    did coding for docker part

--- CertifiedVault/truffle-config.js
+++ CertifiedVault/truffle-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      // host: "127.0.0.1",
+      host: "ganache",
       port: 8545,
       network_id: "*",
     },
